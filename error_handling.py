bcd1c3e76ce3f8f4b9eede7e557ad90a0453ba52
Sukhendra <developer.sukhendra@gmail.com>
2026-08-13 01:13:22 +0530

    error handling

---
+++ error_handling.py
@@ -0,0 +1,22 @@
+# try:
+#     number = int(input("Enter the number: "))
+#     print(f"You entered {number}")
+# except ValueError:
+#     print("That is not a valid number")
+
+
+# ValueError      # wrong type of value, e.g. int("abc")
+# ZeroDivisionError  # dividing by zero
+# TypeError       # wrong type used in an operation, e.g. "5" + 5
+# KeyError        # accessing a dict key that doesn't exist
+# IndexError      # accessing a list index that doesn't exist
+# FileNotFoundError  # trying to open a file that doesn't exist
+
+
+try:
+    result = 10 / int(input("Enter a divisor: "))
+    print(result)
+except ValueError:
+    print("Please enter a valid number")
+except ZeroDivisionError:
+    print("Can not divided by zero")
