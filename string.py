e2318ba164ecedbfb1039181c95d32100833a333
Sukhendra <developer.sukhendra@gmail.com>
2026-08-13 16:19:52 +0530

    strings

---
+++ string.py
@@ -0,0 +1,34 @@
+# text = "Hello World"
+# name = "sukhendra singh lodhi"
+
+# print(text.upper())
+# print(text.lower())
+# print(name.title())
+# print(name.capitalize())
+
+# Whitespace & cleaning
+
+# text1 = "   Hello World   "
+# print(text1)
+# print(text1.strip())
+# print(text1.lstrip())
+# print(text1.rstrip())
+
+# Searching & checking
+# text = "Hello World"
+
+# print(text.find("World"))
+# print("World" in text)
+# print("Sanju" in text)
+# print(text.startswith("Hello"))
+# print(text.startswith("Sanju"))
+# print(name.endswith("lodhi"))
+# print(name.endswith("Hello"))
+# print(text.count("o"))
+# print(name.count("o"))
+
+# Replacing
+
+text = "I like cats"
+new_text = text.replace("cat", "dog")
+print(new_text)
